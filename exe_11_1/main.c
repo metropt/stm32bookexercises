d57937c9a891bbe06a167f0ef9e27207947da287
José Xavier <josescxavier@gmail.com>
2013-07-11 00:22:38 +0100

    exe_11_1

--- exe_11_1/main.c
+++ exe_11_1/main.c
@@ -2,31 +2,47 @@
 #include <stm32f10x_gpio.h>
 #include <stm32f10x_rcc.h>
 #include <stm32f10x_tim.h>
+#include <misc.h>
 
-void Delay(uint32_t nTime);
+static __IO uint8_t ledval;
+static __IO uint8_t change;
 
 int main(void)
 {
-    
-    if(SysTick_Config(SystemCoreClock / 1000))
-      while(1);
+    ledval=0;
+    change=0;
     
     GPIO_InitTypeDef  GPIO_InitStructure;
     TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
     TIM_OCInitTypeDef TIM_OCInitStrucuture;
 
-    //enable timer clock
+    //Configure TIM2 clock
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
 
-    //enable gpioa
+    //Configure GPIOA
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 
-    // GPIO PA1
+    //Configure GPIO PA1
     GPIO_StructInit(&GPIO_InitStructure);
+
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    //Configure GPIO PA2
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    //configure NVIC
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 
     //configure timer
     //PWM frequency = 100hz with 24,000,000hz system clock
@@ -45,27 +61,29 @@ int main(void)
     TIM_OCInitStrucuture.TIM_OutputState = TIM_OutputState_Enable;
     TIM_OC2Init(TIM2, &TIM_OCInitStrucuture);
 
+    TIM_SetCompare2(TIM2, 499);
+
     TIM_Cmd(TIM2, ENABLE);
     
-    TIM_SetCompare2(TIM2, 499); //50%
-    
     while(1){
+        if(change){
+            GPIO_WriteBit(GPIOA, GPIO_Pin_2, (ledval)? Bit_SET : Bit_RESET);
+            ledval = 1 - ledval;
+            change=0;
+        }
+
     };
 
     return 0;
 }
 
-static __IO uint32_t TimingDelay;
+void TIM2_IRQHandler(void){
 
-void Delay(uint32_t nTime){
-  TimingDelay = nTime;
-  while(TimingDelay != 0);
+    change=1;
+
+    TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
 }
 
-void SysTick_Handler(void){
-  if(TimingDelay != 0x00)
-    TimingDelay--;
-}
 
 #ifdef USE_FULL_ASSERT
 void assert_failed(uint8_t* file, uint32_t line)
